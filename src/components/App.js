6e510228746293cd79eee736a76e29b3fb231921
joaohdallago <joaohdallago@gmail.com>
2021-12-08 18:18:23 -0300

    home layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
 export default function App() {
   return (
-    <div>
-      lalakdasksdsas
+    <div className="container">
+      <img src="assets/logo.png" alt="logo" className="logo" />
+
+      <button>
+        <div>
+          Praticar react
+          <img src="assets/next.png" alt="next icon"/>
+        </div>
+      </button>
     </div>
   );
 }
